763462e098bfdbccf23cfcd8e11f8d7809cc7c17
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-03-01 13:37:43 -0500

    added 'val' object to be the value of the hashmap (since tuple does not exsist in java)

---
+++ src/main/java/ValueObject.java
@@ -0,0 +1,26 @@
+public class ValueObject {
+
+//    String documentFoundIn;
+//    Integer position;
+    String[] val = new String[2];
+
+    ValueObject(String documentFoundIn, Integer position){
+//        this.documentFoundIn = documentFoundIn;
+//        this.position = position;
+        this.val = new String[]{documentFoundIn, position.toString()};
+    }
+//
+//    public int getPosition(){
+//        return position;
+//    }
+//
+//    public String getDocumentFoundIn(){
+//        return documentFoundIn;
+//    }
+
+    public void print(){
+        System.out.println(val[0] + "  " + val[1]);
+    }
+
+
+}
--- src/main/java/wHashTable.java
+++ src/main/java/wHashTable.java
@@ -5,9 +5,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class fromMultiple {
-
+public class wHashTable {
     public static void main(String[] args) throws IOException {
+
+        ValueObject val = new ValueObject("testString", 0);
+
+        val.print();
 
         // Create path to file directory
         File dir =  new File("src/main/resources/");
@@ -24,20 +27,19 @@ public class fromMultiple {
 
         // Read html file
         for (File curr: dir.listFiles()){
-           File input = new File(curr.toString());
-           // Parse text
-           Document page = Jsoup.parse(input, "UTF-8");
-           // get only text
-           String allWords = page.text();
+            File input = new File(curr.toString());
+            // Parse text
+            Document page = Jsoup.parse(input, "UTF-8");
+            // get only text
+            String allWords = page.text();
 
             // Split allWords string into an array. Each word is an element in the new array.
             for( String e: allWords.split(" ")){
-               wordsInList.add(e);
-           }
+                wordsInList.add(e);
+            }
         }
 
         //Show length -- currently now currently 1668
         System.out.println(wordsInList.size());
-
     }
 }

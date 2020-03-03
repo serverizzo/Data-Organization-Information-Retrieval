f5dfabdb914c68ff0b65a09e6ab6646d936c48c2
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-03-03 09:31:58 -0500

    added sudocode to stowordsHashTable

--- src/main/java/StopWordHashTable.java
+++ src/main/java/StopWordHashTable.java
@@ -1,7 +1,16 @@
 public class StopWordHashTable {
 
+    // What datatype is our stopword list going to be?
+    // stopwords
 
-    public StopWordHashTable{
+    public StopWordHashTable{}
+
+    public void readInStopWordObject(){
+
+    }
+
+    // Detects if word is in stopwords
+    public boolean contains(String word){
 
     }
 

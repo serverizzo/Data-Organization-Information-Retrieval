73f81e85bf23dbeb68cee0cda2e237e0553cae25
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2020-03-02 19:13:38 -0500

    changed MainClass to BuildInvertedIndex

--- src/main/java/BuildInvertedIndex.java
+++ src/main/java/BuildInvertedIndex.java
@@ -10,35 +10,41 @@ import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.Map;
 
-public class MainClass {
+public class BuildInvertedIndex {
 
     public static void main(String[] args) throws IOException {
 
-        InvertedIndex invertedIdx = new InvertedIndex();
-        MyParser p = new MyParser();
         // Path to all htmlfiles
         File dir =  new File("src/main/resources/");
+        InvertedIndex invertedIdx = new InvertedIndex();
+        MyParser p = new MyParser();
         int positionCount;
         String allWords;
         PorterStemmer ps = new PorterStemmer();
 
         // Read all html files in 'dir' directory
         for (File curr: dir.listFiles()){
+
+            // Get the next HTML doc
             File input = new File(curr.toString());
             // get only the text from the html --Needs to be Refined--
             allWords = p.parseToString(curr);
+            // Reset count to zero
             positionCount = 0;
-            System.out.println("All words from html: " +  allWords);
+
+            // ---Debuging---
+            // System.out.println("All words from html: " +  allWords);
+
             // Split allWords string into an array. Each word is an element in the new array.
             String[] allWordsArray = allWords.split( " ");
-            for( String keyWord: allWordsArray){
+            for( String keyWord: allWordsArray ){
 
-                //Split the String into an array, each value is a word, catalog it and its index number.
+                // Stem keyWord
                 keyWord = ps.stem(keyWord);
+                // Create Value object
                 ValueObject val = new ValueObject(curr.toString(), positionCount);
+                // Add key and value to inverted index
                 invertedIdx.add(keyWord,val);
-
-                // TODO Currently we are overwriting each put, we need to append to each.
                 positionCount += 1;
             }
         }
